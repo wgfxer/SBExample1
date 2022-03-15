a35b6f55dac792b506116a7f5d331b62bc117900
Миннулин Валерий Русланович <vrminnulin@sberbank.ru>
2022-03-15 17:11:47 +0300

    new commit

--- app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -60,3 +60,12 @@ object UserHolder {
 
     private fun String?.orNull() = if (this.isNullOrEmpty()) null else this
 }
+
+fun String?.clearPhone() = this?.replace("""[^+\d]""".toRegex(), "")
+
+fun String?.rawPhoneValid(): Boolean {
+    if (this == null) return false
+    return this.matches("""[^A-Za-z]*""".toRegex()) && this.clearPhone()?.matches("""\+\d{11}""".toRegex()).orFalse()
+}
+
+fun Boolean?.orFalse() = this ?: false
