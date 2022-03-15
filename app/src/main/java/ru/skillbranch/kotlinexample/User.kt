c065e5c15e618538a0dd2ce018aa5b2253f53797
Миннулин Валерий Русланович <vrminnulin@sberbank.ru>
2022-03-15 16:54:33 +0300

    init commit

---
+++ app/src/main/java/ru/skillbranch/kotlinexample/User.kt
@@ -0,0 +1,190 @@
+package ru.skillbranch.kotlinexample
+
+import androidx.annotation.VisibleForTesting
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.security.SecureRandom
+
+class User private constructor(
+        private val firstName: String,
+        private val lastName: String?,
+        email: String? = null,
+        rawPhone: String? = null,
+        meta: Map<String, Any>? = null
+) {
+    val userInfo: String
+
+    private val fullName: String
+        get() = listOfNotNull(firstName, lastName)
+                .joinToString(" ")
+                .capitalize()
+
+    private val initials: String
+        get() = listOfNotNull(firstName, lastName)
+                .map { it.first().toUpperCase() }
+                .joinToString(" ")
+
+    private var phone: String? = null
+        set(value) {
+            field = value.clearPhone()
+        }
+
+    private var _login: String? = null
+    var login: String
+        set(value) {
+            _login = value.toLowerCase()
+        }
+        get() = _login!!
+
+    private var salt: String? = null
+
+    private lateinit var passwordHash: String
+
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    var accessCode: String? = null
+
+    // For email
+    constructor(
+            firstName: String,
+            lastName: String?,
+            email: String,
+            password: String?,
+            fromCsv: Boolean,
+            passData: Pair<String, String>? = null
+    ) : this(firstName, lastName, email = email, meta = mapOf("auth" to "password").addCsvIfNeed(fromCsv)) {
+        println("Secondary email constructor")
+        if (passData == null) {
+            passwordHash = encrypt(password!!)
+        } else {
+            salt = passData.first
+            passwordHash = passData.second
+        }
+    }
+
+    // For phone
+    constructor(
+            firstName: String,
+            lastName: String?,
+            rawPhone: String,
+            fromCsv: Boolean,
+            passData: Pair<String, String>? = null
+    ) : this(firstName, lastName, rawPhone = rawPhone, meta = mapOf("auth" to "sms").addCsvIfNeed(fromCsv)) {
+        println("Secondary phone constructor")
+        if (passData == null) {
+            val code = generateAccessCode()
+            passwordHash = encrypt(code)
+            println("Phone passwordHash is $passwordHash")
+            accessCode = code
+            sendAccessCodeToUser(rawPhone, code)
+        } else {
+            salt = passData.first
+            passwordHash = passData.second
+        }
+    }
+    
+    init {
+        println("First init block, primary constructor was called")
+
+        check(firstName.isNotBlank()) { "FirstName must not be blank" }
+        check(!email.isNullOrBlank() || !rawPhone.isNullOrBlank()) { "Email or phone must not be null or blank" }
+
+        phone = rawPhone
+        login = email ?: phone!!
+
+        userInfo = """
+            firstName: $firstName
+            lastName: $lastName
+            login: $login
+            fullName: $fullName
+            initials: $initials
+            email: $email
+            phone: ${phone}
+            meta: $meta
+        """.trimIndent()
+    }
+
+    fun checkPassword(pass: String) = encrypt(pass) == passwordHash.also {
+        println("Checking passwordHash is $passwordHash")
+    }
+
+    fun changePassword(oldPass: String, newPass: String) {
+        if (checkPassword(oldPass)) {
+            passwordHash = encrypt(newPass)
+            if (!accessCode.isNullOrEmpty()) accessCode = newPass
+            println("Password $oldPass has been changed on new password $newPass")
+        } else throw IllegalArgumentException("The entered password does not match the current password")
+    }
+
+    private fun encrypt(password: String): String {
+        if (salt.isNullOrEmpty()) {
+            salt = ByteArray(16).also { SecureRandom().nextBytes(it) }.toString()
+        }
+        println("Salt while encrypt: $salt")
+        return salt.plus(password).md5()
+    }
+
+    private fun String.md5(): String {
+        val md = MessageDigest.getInstance("MD5")
+        val digest = md.digest(toByteArray()) // 16 byte
+        val hexString = BigInteger(1, digest).toString(16)
+        return hexString.padStart(32, '0')
+    }
+
+    fun generateAccessCode(): String {
+        val possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+        return StringBuilder().apply {
+            repeat(6) {
+                (possible.indices).random().also { index ->
+                    append(possible[index])
+                }
+            }
+        }.toString()
+    }
+
+    fun sendAccessCodeToUser(phone: String, code: String) {
+        println("..... sending access code: $code on $phone")
+    }
+
+    companion object Factory {
+        fun makeUser(
+                fullName: String,
+                email: String? = null,
+                password: String? = null,
+                phone: String? = null,
+                isFromCsv: Boolean = false,
+                passData: Pair<String, String>? = null
+        ): User {
+            val (firstName, lastName) = fullName.fullNameToPair()
+
+            return when {
+                !phone.isNullOrBlank() -> User(firstName, lastName, phone, isFromCsv, passData)
+                !email.isNullOrBlank() -> User(
+                    firstName,
+                    lastName,
+                    email,
+                    password,
+                    isFromCsv,
+                    passData
+                )
+                else -> throw IllegalArgumentException("Email or phone must not be null or blank")
+            }
+        }
+
+        private fun String.fullNameToPair(): Pair<String, String?> =
+                this.split(" ")
+                        .filter { it.isNotBlank() }
+                        .run {
+                            when (size) {
+                                1 -> first() to null
+                                2 -> first() to last()
+                                else -> throw IllegalArgumentException(
+                                        "FullName must contain only first name and last name, current split " +
+                                                "result: ${this@fullNameToPair}"
+                                )
+                            }
+                        }
+    }
+}
+
+private fun Map<String, Any>.addCsvIfNeed(fromCsv: Boolean) = if (fromCsv) toMutableMap().apply { this["src"] = "csv" } else this
---
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -0,0 +1,62 @@
+package ru.skillbranch.kotlinexample
+
+import androidx.annotation.VisibleForTesting
+
+object UserHolder {
+    private val map = mutableMapOf<String, User>()
+
+    fun registerUser(
+            fullName: String,
+            email: String,
+            password: String
+    ): User {
+        if (map.contains(email.lowercase())) throw IllegalArgumentException("A user with this email already exists")
+        return User.makeUser(fullName, email = email, password = password)
+            .also { user -> map[user.login] = user }
+    }
+
+    fun loginUser(login: String, password: String): String? {
+        val userByEmail = map[login.trim()]
+        val userByPhone = map[login.clearPhone()]
+        val user = userByEmail ?: userByPhone ?: return null
+        return if (user.checkPassword(password)) user.userInfo else null
+    }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    fun clearHolder() {
+        map.clear()
+    }
+
+    fun requestAccessCode(login: String) {
+        val user = map[login.clearPhone()] ?: return
+        val newAccessCode = user.generateAccessCode()
+        user.accessCode?.let { oldAccessCode -> user.changePassword(oldAccessCode, newAccessCode) }
+    }
+
+    fun registerUserByPhone(fullName: String, rawPhone: String): User {
+        if (map.contains(rawPhone.clearPhone())) throw IllegalArgumentException("A user with this phone already exists")
+        if (!rawPhone.rawPhoneValid()) throw IllegalArgumentException("Enter a valid phone number starting with a + and containing 11 digits")
+        return User.makeUser(fullName = fullName, phone = rawPhone)
+            .also { user -> map[user.login] = user}
+    }
+
+    fun importUsers(list: List<String>) = list.map { convertToUser(it) }
+
+    private fun convertToUser(stringUser: String): User {
+        val fields: List<String> = stringUser.split(';')
+        val userFullName = fields[0]
+        val userEmail = fields[1].orNull()
+        val userPasswordData = fields[2].split(':').let { it[0] to it[1] }
+        val userPhone = fields[3].orNull()
+        return User.makeUser(
+            userFullName,
+            email = userEmail,
+            password = null,
+            phone = userPhone,
+            isFromCsv = true,
+            passData = userPasswordData
+        )
+    }
+
+    private fun String?.orNull() = if (this.isNullOrEmpty()) null else this
+}
